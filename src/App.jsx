5dfe17eb5d4f21102c6ec152cb56239589abbdd0
rigby200602 <tungle3456@gmail.com>
2025-03-06 00:13:40 +0700

    update sidebar css and add sidebar to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
     <div className='bg-black h-screen'>
       <div className='h-[90%]'>
-
+        <Sidebar/>
       </div>
     </div>
   )
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
+      
+    </div>
+  )
+}
+
+export default Sidebar
