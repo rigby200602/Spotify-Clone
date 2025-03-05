ca6b8654cc9f6e6214d81a8b08911711d698bd66
rigby200602 <tungle3456@gmail.com>
2025-03-06 00:34:52 +0700

    home icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {assets} from '../assets/assets'
 
 const Sidebar = () => {
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
-      
+      <div className='bg-[#121212] h-[12%] flex flex-col rounded justify-around'>
+        <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+            <img src={assets.home_icon} alt="Home" />
+        </div>
+      </div>
     </div>
   )
 }
