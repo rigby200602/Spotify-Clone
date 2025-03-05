eb7417ee0f61b054bc6cda9e2e824ed7fac24f7d
rigby200602 <tungle3456@gmail.com>
2025-03-06 01:04:38 +0700

    add library

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,12 +10,17 @@ const Sidebar = () => {
             <p className='font-bold'>Home</p>
         </div>
         <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-            <img className='w-6' src={assets.search_icon} alt="Home" />
+            <img className='w-6' src={assets.search_icon} alt="Search" />
             <p className='font-bold'>Search</p>
         </div>
       </div>
       <div className='bg-[#121212] h-[85%] rounded-sm'>
-
+        <div className='p-4 flex items-center justify-between'>
+            <div className='flex items-center gap-3'>
+                <img className='w-8' src={assets.stack_icon} alt="Stack" />
+                <p className='font-semibold'>Your Library</p>
+            </div>
+        </div>
       </div>
     </div>
   )
