534a9ba688502c9ec22cbf017486099d17a8401f
rigby200602 <tungle3456@gmail.com>
2025-03-06 00:54:10 +0700

    add new zone

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../assets/assets'
 const Sidebar = () => {
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
-      <div className='bg-[#121212] h-[12%] flex flex-col rounded justify-around'>
+      <div className='bg-[#121212] h-[12%] flex flex-col rounded-sm justify-around'>
         <div className='flex items-center gap-3 pl-8 cursor-pointer'>
             <img className='w-6' src={assets.home_icon} alt="Home" />
             <p className='font-bold'>Home</p>
@@ -13,6 +13,9 @@ const Sidebar = () => {
             <img className='w-6' src={assets.search_icon} alt="Home" />
             <p className='font-bold'>Search</p>
         </div>
+      </div>
+      <div className='bg-[#121212] h-[85%] rounded-sm'>
+
       </div>
     </div>
   )
