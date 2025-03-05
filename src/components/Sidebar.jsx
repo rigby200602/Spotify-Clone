cbbb2ae7c4e6cf77311f9f301ffd027b56e6a2ce
rigby200602 <tungle3456@gmail.com>
2025-03-06 00:47:42 +0700

    search icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,12 @@ const Sidebar = () => {
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
       <div className='bg-[#121212] h-[12%] flex flex-col rounded justify-around'>
         <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-            <img src={assets.home_icon} alt="Home" />
+            <img className='w-6' src={assets.home_icon} alt="Home" />
+            <p className='font-bold'>Home</p>
+        </div>
+        <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+            <img className='w-6' src={assets.search_icon} alt="Home" />
+            <p className='font-bold'>Search</p>
         </div>
       </div>
     </div>
