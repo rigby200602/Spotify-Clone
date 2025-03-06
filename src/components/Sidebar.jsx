a87da861c57400c94bce2380961e268e15fa8b19
rigby200602 <tungle3456@gmail.com>
2025-03-06 11:14:14 +0700

    add cmt

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,7 @@ import {assets} from '../assets/assets'
 const Sidebar = () => {
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
+      {/* Home & Search */}
       <div className='bg-[#121212] h-[12%] flex flex-col rounded-sm justify-around'>
         <div className='flex items-center gap-3 pl-8 cursor-pointer'>
             <img className='w-6' src={assets.home_icon} alt="Home" />
@@ -14,6 +15,7 @@ const Sidebar = () => {
             <p className='font-bold'>Search</p>
         </div>
       </div>
+      {/* Show your Library */}
       <div className='bg-[#121212] h-[85%] rounded-sm'>
         <div className='p-4 flex items-center justify-between'>
             <div className='flex items-center gap-3'>
@@ -25,6 +27,7 @@ const Sidebar = () => {
               <img className='w-5' src={assets.plus_icon} alt="plus" />
             </div>
         </div>
+        {/* Create playlist and browse podcasts */}
         <div className='p-4 bg-[#242424] m-2 rounded-sm font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
             <h1>Create your first playlist</h1>
             <p className='font-light'>It's easy we will help you</p>
