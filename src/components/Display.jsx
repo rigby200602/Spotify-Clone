69a64c04d384085853e3aacd66cfafad621a3a66
rigby200602 <tungle3456@gmail.com>
2025-03-06 23:43:50 +0700

    update display css

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Display = () => {
   return (
     <div>
-      
+      <div className='w-[100%] m-2 px-6 pt-4 rounded-sm bg-[#121212] text-white overflow-auto lg:w-[25%] lg:ml-0'>
+
+      </div>
     </div>
   )
 }
