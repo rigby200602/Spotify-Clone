5209a5df19c38f0d21ce71b7187b213f146285d9
rigby200602 <tungle3456@gmail.com>
2025-03-06 11:03:02 +0700

    add Create Playlist and browse podcast

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,6 +20,20 @@ const Sidebar = () => {
                 <img className='w-8' src={assets.stack_icon} alt="Stack" />
                 <p className='font-semibold'>Your Library</p>
             </div>
+            <div className='flex items-center gap-3'>
+              <img className='w-5' src={assets.arrow_icon} alt="arrow" />
+              <img className='w-5' src={assets.plus_icon} alt="plus" />
+            </div>
+        </div>
+        <div className='p-4 bg-[#242424] m-2 rounded-sm font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+            <h1>Create your first playlist</h1>
+            <p className='font-light'>It's easy we will help you</p>
+            <button className='px-4 py-1.5 bg-white text-[20px] text-black rounded-full mt-4 cursor-pointer'>Create playlist</button>
+        </div>
+        <div className='p-4 bg-[#242424] m-2 rounded-sm font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
+            <h1>Let's find some popcost</h1>
+            <p className='font-light'>We'll keep you update on new episodes</p>
+            <button className='px-4 py-1.5 bg-white text-[20px] text-black rounded-full mt-4 cursor-pointer'>Browse podcasts</button>
         </div>
       </div>
     </div>
