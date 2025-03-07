5ffd6755e5fb12169192f55341b9352758226d02
rigby200602 <tungle3456@gmail.com>
2025-03-07 16:47:34 +0700

    show info about album

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData } from '../assets/assets';
+import { albumsData, assets } from '../assets/assets';
 
 const DisplayAlbum = () => {
     
@@ -13,7 +13,19 @@ const DisplayAlbum = () => {
     <>
       <Navbar/>
       <div className='flex mt-10 gap-8 flex-col md:flex-row md:items-end '>
-        <img src={albumData.image} alt="album image" />
+        <img className='w-48 rounded-sm' src={albumData.image} alt="album image" />
+        <div className='flex flex-col'>
+            <p>Playlist</p>
+            {/* get album name */}
+            <h2 className='text-[5px] font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
+            <h4>{albumData.desc}</h4>
+            <p className='mt-1'>
+                <img className='inline-block w-5 m-2' src={assets.spotify_logo} alt="logo" />
+                <strong>Spotify</strong>
+                . 1,232,085 likes
+                . <strong>50 songs,</strong> about 2 hr 30 mins
+            </p>
+        </div>
       </div>
     </>
   )
