48328705b2b545188302ccfff6b78b2a3cd722c6
rigby200602 <tungle3456@gmail.com>
2025-03-07 18:41:18 +0700

    building changing bg color base for each album

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route, useLocation } from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
+import { useRef } from 'react'
 
 const Display = () => {
+  
+  const displayRef = useRef();
+  // get location
+  const location = useLocation();
+  // check if album size or not
+  const isAlbum = location.pathname.includes('album');
+  
+
   return (
-      <div className='w-full h-[96%] m-2 px-6 pt-4 rounded-sm bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
+      <div ref={displayRef} className='w-full h-[96%] m-2 px-6 pt-4 rounded-sm bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
         <Routes>
             <Route path='/' element={<DisplayHome/>}></Route>
             <Route path='/album/:id' element={<DisplayAlbum/>}></Route>
