b6430b14a663b8c8013e2f4777bc3ec883ec8a71
rigby200602 <tungle3456@gmail.com>
2025-03-07 14:26:22 +0700

    build album component

--- src/components/AlbumItems.jsx
+++ src/components/AlbumItems.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const AlbumItems = () => {
+
+const AlbumItems = ({image,id,name,desc}) => {
   return (
-    <div>
-      
+    <div className='min-w-[180px p-2 px-3 rounded-sm cursor-pointer hover:bg-[#ffffff26]'>
+      <img className='rounded-sm' src={image} alt="" />
+      <p className='font-bold mt-2 mb-1'>{name}</p>
+      <p className='text-slate-200 text-sm'>{desc}</p>
     </div>
   )
 }
