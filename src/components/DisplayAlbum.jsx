44ba352ac55a2bf4d810e2297a0a679eaef71533
rigby200602 <tungle3456@gmail.com>
2025-03-07 16:36:12 +0700

    show album image

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Navbar from './Navbar'
+import { useParams } from 'react-router-dom'
+import { albumsData } from '../assets/assets';
 
 const DisplayAlbum = () => {
-  return (
-    <div>
-      
-    </div>
+    
+    const {id} = useParams();
+    // get data from albumsData through id
+    const albumData = albumsData[id]
+    
+    return (
+    <>
+      <Navbar/>
+      <div className='flex mt-10 gap-8 flex-col md:flex-row md:items-end '>
+        <img src={albumData.image} alt="album image" />
+      </div>
+    </>
   )
 }
 
--- src/components/SongItems.jsx
+++ src/components/SongItems.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SongItems = ({image,id,name,desc}) => {
     return (
     <div className='min-w-[180px] p-2 px-3 rounded-sm cursor-pointer hover:bg-[#ffffff26]'>
-      <img className='rounded-sm' src={image} alt="" />
+      <img className='rounded-sm' src={image} alt="Song Image" />
       <p className='font-bold mt-2 mb-1'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
     </div>
