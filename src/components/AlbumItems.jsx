84d819aa2e545cc1c11949963c80ff755e78a02e
rigby200602 <tungle3456@gmail.com>
2025-03-07 15:25:23 +0700

    access to album through id

--- src/components/AlbumItems.jsx
+++ src/components/AlbumItems.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { useNavigation } from 'react-router-dom'
 
 const AlbumItems = ({image,id,name,desc}) => {
-  return (
-    <div className='min-w-[180px] p-2 px-3 rounded-sm cursor-pointer hover:bg-[#ffffff26]'>
+  
+    const navigation = useNavigation()
+
+  
+    return (
+    <div onClick={() => navigation(`/album/${id}`)} className='min-w-[180px] p-2 px-3 rounded-sm cursor-pointer hover:bg-[#ffffff26]'>
       <img className='rounded-sm' src={image} alt="" />
       <p className='font-bold mt-2 mb-1'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
--- src/components/SongItems.jsx
+++ src/components/SongItems.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { useNavigation } from 'react-router-dom'
 
 const SongItems = ({image,id,name,desc}) => {
-  return (
+    return (
     <div className='min-w-[180px] p-2 px-3 rounded-sm cursor-pointer hover:bg-[#ffffff26]'>
       <img className='rounded-sm' src={image} alt="" />
       <p className='font-bold mt-2 mb-1'>{name}</p>
