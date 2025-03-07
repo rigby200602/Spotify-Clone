24b3af6e7d3365b9d0860a2e84fb5074a1eafbbf
rigby200602 <tungle3456@gmail.com>
2025-03-07 18:35:35 +0700

    btn left right function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+  
+  const navigate = useNavigate();
+  
   return (
     <>
       {/* Left and right btn */}
       <div className='w-full flex justify-between items-center font-semibold'>
         <div className='flex items-center gap-2'>
-          <img className='w-8 cursor-pointer bg-black rounded-2xl p-2' src={assets.arrow_left} alt="Arrow Left" />
-          <img className='w-8 cursor-pointer bg-black rounded-2xl p-2' src={assets.arrow_right} alt="Arrow Right" />
+          <img onClick={()=>navigate(-1)} className='w-8 cursor-pointer bg-black rounded-2xl p-2' src={assets.arrow_right} alt="Arrow Right" />
+          <img onClick={()=>navigate(+1)} className='w-8 cursor-pointer bg-black rounded-2xl p-2' src={assets.arrow_left} alt="Arrow Left" />
         </div>
         {/* explore, install, username */}
         <div className='flex items-center gap-4'>
