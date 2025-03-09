c4f79a6674b4a75d1f6a345b022092f0ffe875ae
rigby200602 <tungle3456@gmail.com>
2025-03-09 12:14:22 +0700

    tracking time, check if play or not, time, play and pause method

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -8,13 +8,42 @@ const PlayerContextProvider = (props) => {
     const audioRef = useRef();
     const seekBg = useRef();
     const seekBar = useRef();
-
+    // Tracking time
     const [track,setTrack] = useState(songsData[0]);
+    // play or not
+    const [playStatus,setPlayStatue] = useState(false);
+    //Time of song
+    const [time, setTime] = use({
+        currentTime: {
+            second:0,
+            minute:0
+        },
+        totalTime: {
+            second: 0,
+            minute: 0
+        }
+    }) 
+    // Play method
+    const play = () => {
+        audioRef.current.play()
+        setPlayStatue(true)
+    }
+    // Pause method
+    const pause = () => {
+        audioRef.current.pause()
+        setPlayStatue(false)
+    }
 
     const contextValue = {
         audioRef,
         seekBg,
-        seekBar
+        seekBar,
+        track,
+        setTrack,
+        playStatus,
+        setPlayStatue,
+        time,
+        setTime,
     }
 
     return (
