bc27d8829ce2875c27f1528d3aa10804ddd4e43f
rigby200602 <tungle3456@gmail.com>
2025-03-12 16:09:41 +0700

    back to home

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import {assets} from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Sidebar = () => {
+  
+  const navigate = useNavigate()
+
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
       {/* Home & Search */}
       <div className='bg-[#121212] h-[12%] flex flex-col rounded-sm justify-around'>
-        <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+        {/* use navigate to back to home */}
+        <div onClick={() => navigate('/')} className='flex items-center gap-3 pl-8 cursor-pointer'>
             <img className='w-6' src={assets.home_icon} alt="Home" />
             <p className='font-bold'>Home</p>
         </div>
