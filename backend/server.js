508c3b51961af9de85c24546990309c43bb35db7
rigby200602 <tungle3456@gmail.com>
2025-03-12 19:52:07 +0700

    songRouter

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import songRouter from './src/routes/songRoute';
 
 // app config
 const app = express();
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(cors());
 
 // intializing routes
+app.use('/api/song', songRouter)
+
 app.get('/', (req,res) => { res.send("API Working") })
 
 app.listen(port, () => console.log(`Server started on ${port}`))
--- backend/src/routes/songRoute.js
+++ backend/src/routes/songRoute.js
@@ -1,1 +1,9 @@
 import { addSong, listSong } from "../controller/songController";
+import express from 'express'
+
+const songRouter = express.Router();
+
+songRouter.post('/add', addSong);
+songRouter.get('/list', listSong);
+
+export default songRouter;
