6c781febe0991530203be2d10d233fbd73721eab
rigby200602 <tungle3456@gmail.com>
2025-03-12 19:59:03 +0700

    test check connect

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 
+    mongoose.connection.on('connected',() =>{
+        console.log('test');
+        
+    })
     await mongoose.connect(`${process.env.MONGODB_URI}/spotify`)
 }
 
