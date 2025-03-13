d1ec615cf9f2af1457c9cda7646a96b2004d0bc8
rigby200602 <tungle3456@gmail.com>
2025-03-13 23:15:45 +0700

    multer ( for storge and multer )

--- backend/src/models/albumModel.js
+++ backend/src/models/albumModel.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 // create album schema
 const albumSchema = new mongoose({
-    name: { type: String, require: true },
-    desc: { type: String, require: true },
-    bgColor: { type: String, require: true },
-    image: { type: String, require: true }
+    name: { type: String, required: true },
+    desc: { type: String, required: true },
+    bgColor: { type: String, required: true },
+    image: { type: String, required: true }
 })
 
 const albumModel = new mongoose.model.album || mongoose.model("album", albumSchema)
